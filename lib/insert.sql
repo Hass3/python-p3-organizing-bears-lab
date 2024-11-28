6547870fb1e3023c3a31def0213dc14d7d2a3441
Hass <hassmroueh@gmail.com>
2024-11-27 19:07:04 -0500

    organizing bears lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate', 4, 'M', 'brown','hungry', 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy', 6, 'M', 'brown','hungry', 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha', 10, 'F', 'Black','hungry', 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown', 5, 'M', 'brown','hungry', 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa', 4, 'F', 'White','Scared', 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch', 11, 'M', 'Green','Greedy', 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy', 12, 'F', 'brown','hungry', 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL, 4, 'M', 'brown','hungry', 0);
